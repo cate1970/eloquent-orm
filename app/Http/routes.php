16783d41c7f21faf73d56495d1eb036f0177491a
charlie <carlos.teran.gutierrez@gmail.com>
2017-07-21 21:47:40 -0500

    Install Bootstrap

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,39 +13,5 @@
 use App\User;
 use Faker\Factory as Faker;
 
-Route::get('/create', function () {
-	$faker = Faker::create();
+Route::get('/', 'PagesController@home');
 
-	$user = User::create(['name' => $faker->name, 
-		'email' => $faker->email, 
-		'Password' => bcrypt('123456'),
-		'gender' => $faker->randomElement(['f', 'm']),
-		'biography' => $faker->text(255)
-		]);
-
-	return $user;
-});
-
-Route::get('/read/{id}', function($id){
-	$user = User::find($id);
-	return $user;
-});
-
-Route::get('/update/{id}', function ($id) {
-	$faker = Faker::create();
-
-	$user = User::find($id);
-	$user->name = $faker->name;
-	$user->gender =  $faker->randomElement(['f', 'm']);
-	$user->biography= $faker->text(255);
-	$user->save();
-	return $user;
-
-});
-
-Route::get('/delete/{id}', function($id){
-	$user = User::find($id);
-	$user->delete();
-
-	return 'Usuario eliminado';
-});
